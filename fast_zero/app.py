8d0f5031189b526f47737ccb4b651cbd0acbc9ae
lhsantos-rbr <ls@rbrasset.com.br>
2025-06-09 13:57:06 -0300

    continuando aula 03

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from http import HTTPStatus
+from fast_zero.schemas import Message, UserSchema, UserPublic
 
 app = FastAPI()  
 
-@app.get('/')  
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)  
 def read_root():  
     return {'message': 'Olá Mundo!'}
+
+@app.post('/users/', response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
---
+++ fast_zero/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+class Message(BaseModel):
+    message: str
+
+class UserSchema(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class UserPublic(BaseModel):
+    username: str
+    email: str
